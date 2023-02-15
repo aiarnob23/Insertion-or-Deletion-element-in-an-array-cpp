95ac789b5a693959089c79ce2a0426c5a7dd5aa8
aiarnob23 <aiarnob23@gmail.com>
2023-02-15 10:09:17 +0600

    cmnts added

--- code.cpp
+++ code.cpp
@@ -3,6 +3,7 @@ using namespace std;
 const int N=1e7;
 int arr[N];
 
+//function prototype
 void sortedInsertion(int arr[],int size,int pos,int num);
 void sortedDeletion(int arr[],int size,int pos);
 
@@ -13,19 +14,20 @@ int main()
     int choice,size,i,pos,num;
     cout<<"This code is for sorted array\nEnter your choice\n1.For insertion\n2.For deletion\n3.For exit"<<endl;
     cin>>choice;
-    if(choice!=3){
-         cout<<"Enter the size of the arary"<<endl;
-    cin>>size;
-    cout<<"Enter the elements of the array"<<endl;
-    for(i=0; i<size; i++)
+    if(choice!=3)
     {
-        cin>>arr[i];
-    }
+        cout<<"Enter the size of the arary"<<endl;
+        cin>>size;
+        cout<<"Enter the elements of the array"<<endl;
+        for(i=0; i<size; i++)
+        {
+            cin>>arr[i];
+        }
     }
 
     while (choice!=3)
     {
-       
+
         cout<<"Enter the position of operation"<<endl;
         cin>>pos;
         if(pos<=0 || pos>size)
@@ -64,7 +66,6 @@ int main()
 
     return 0;
 }
-
 
 
 
@@ -113,3 +114,4 @@ void sortedDeletion(int arr[],int size,int pos)
     cout<<"\nThe element is deleted : "<<temp<<"\tFrom position : "<<pos<<"\n\n";
 
 }
+
